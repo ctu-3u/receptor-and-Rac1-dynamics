c42d63e576e3ab902437931f2f577a059440e07c
ctu-3u <lyii.ctu@gmail.com>
2024-10-15 23:24:18 -0400

    Update plot_dynamics_polar.py

--- plot_dynamics_polar.py
+++ plot_dynamics_polar.py
@@ -4,3 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import simu_para as spa
+
+with h5py.File(".\\data\\"+spa.datelabel+"\\Act_Rac.h5",'r') as f:
+    for i in range(0,spa.rounds,spa.sampot):
+        act_rac_dist = f['act_dist_'+str(i)][()]
